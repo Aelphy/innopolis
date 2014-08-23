94c2adfc6470133cfe7c133506adc19f4e084c05
Aelphy <miha-usv@yandex.ru>
2014-08-24 00:49:52 +0400

    feture(programming): adding some code

---
+++ cs_algorithms/Debug/src/cs-as1.d
@@ -0,0 +1,1 @@
+src/cs-as1.d: ../src/cs-as1.c
--- stata/hw1.R
+++ stata/hw1.R
@@ -6,7 +6,7 @@ mean_x = sum(data$X) / n
 mean_y = sum(data$Y) / n
 div_x = sqrt(sum((data$X - mean_x)^2) / (n - 1))
 div_y = sqrt(sum((data$Y - mean_y)^2) / (n - 1))
-r = sum((data$X - mean_x) * (data$Y - mean_y)) / (div_x * div_y * n)
+r = sum((data$X - mean_x) * (data$Y - mean_y)) / (div_x * div_y * (n - 1))
 
 mean(data$X)
 mean(data$Y)
