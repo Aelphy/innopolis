e57020dfd3f708ccba348e2a1654cfc081e596fd
Mikhail Usvyatsov <m.usvyatsov@gmail.com>
2015-03-18 09:25:35 +0300

    sna draft

--- sna/lab3/processor.py
+++ sna/lab3/processor.py
@@ -2,6 +2,7 @@ from datetime import datetime as dt
 
 data_file = open('data.txt', 'rb')
 
+network_list = {}
 network = {}
 people = {}
 
@@ -15,48 +16,111 @@ def process_next_line():
     else:
         return line.split('\t')
 
-is_processed_tuple = False
 
-while not is_processed_tuple:
-    e_list = process_next_line()
+def initialize():
+    global network_list
+    global network
 
-    if e_list:
-        e = int(e_list[1])
-    else:
-        is_processed_tuple = True
-        continue
+    is_processed_tuple = False
 
-    t_list = process_next_line()
-    t = dt.strptime(t_list[1], '%Y-%m-%d %H:%M:%S\t')
+    while not is_processed_tuple:
+        e_list = process_next_line()
 
-    u_list = process_next_line()
-    u = int(u_list[1])
+        if e_list:
+            e = int(e_list[1])
+        else:
+            is_processed_tuple = True
+            continue
 
-    if u_list[2].strip() not in people.values():
-        people[u] = u_list[2].strip()
+        t_list = process_next_line()
+        t = dt.strptime(t_list[1], '%Y-%m-%d %H:%M:%S\t')
 
-    n_list = process_next_line()
-    n = int(n_list[1])
+        u_list = process_next_line()
+        u = int(u_list[1])
 
-    if n_list[2].strip() not in people.values():
-        people[n] = n_list[2].strip()
+        if u_list[2].strip() not in people.values():
+            people[u] = u_list[2].strip()
 
-    network[u] = [{n : 1}]
+        n_list = process_next_line()
+        n = int(n_list[1])
 
-    is_votings_finished = False
-    is_v = False
+        if n_list[2].strip() not in people.values():
+            people[n] = n_list[2].strip()
 
-    while not is_votings_finished:
-        v_list = process_next_line()
+        network_list[u] = {n : 1}
 
-        if v_list[0] == 'V':
-            is_v = True
+        is_votings_finished = False
+        is_v = False
 
-            vote = int(v_list[1])
-            user = int(v_list[2])
+        while not is_votings_finished:
+            v_list = process_next_line()
 
-            network[u].append({user : vote})
-        elif is_v:
-            is_votings_finished = True
+            if v_list[0] == 'V':
+                is_v = True
+
+                vote = int(v_list[1])
+                user = int(v_list[2])
+
+                if user not in network_list[u]:
+                    network_list[u][user] = vote
+            elif is_v:
+                is_votings_finished = True
+
+    for user, votes in network_list.items():
+        for voter, vote in votes.items():
+            if vote != 0 and (voter not in network_list or user not in network_list[voter] or network_list[voter][user] != - vote):
+                if  user not in network:
+                    network[user] = {}
+
+                if  voter not in network:
+                    network[voter] = {}
+
+                if  voter not in network[user]:
+                    network[user][voter] = vote
+
+                if  user not in network[voter]:
+                    network[voter][user] = vote
 
 
+
+def dfs(graph, start, visited):
+    if start in visited:
+        return []
+
+    stack = [start]
+    current = set()
+
+    while stack:
+        vertex = stack.pop()
+
+        if vertex not in visited:
+            visited.add(vertex)
+            current.add(vertex)
+
+            candidates = set()
+
+            if vertex in graph:
+                for u_id, vote in graph[vertex].items():
+                    if vote == 1:
+                        candidates.add(u_id)
+                stack.extend(candidates - visited)
+
+    return current
+
+
+def process():
+    visited = set()
+    supernodes = []
+
+    global network
+    global people
+
+    for u_id, name in people.items():
+        candidate = dfs(network, u_id, visited)
+
+        if candidate:
+            supernodes.append(dfs(network, u_id, visited))
+
+    supernodes = [x for x in supernodes if x]
+
+    return supernodes
