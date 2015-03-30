75d376f3c55915fd3f5deab13c2ac70ef795b8b3
Mikhail Usvyatsov <m.usvyatsov@gmail.com>
2015-03-30 14:06:03 +0300

    fixed perceptron

--- ml/classifiers/perceptron.py
+++ ml/classifiers/perceptron.py
@@ -4,14 +4,13 @@ import random
 
 emails_root = 'Emails'
 
-answers = {'spam-train' : 1, 'nonspam-train' : -1}
+answers = {'spam-train' : -1, 'nonspam-train' : 1}
 
 words = set()
 weights = {}
 features = {}
-b = 0
-examples_count = 1
-error_rate = 0.1
+examples_count = 0
+error_rate = 0.004
 
 files = {'nonspam-train' : os.listdir(os.path.join(emails_root, 'nonspam-train')),
          'spam-train' : os.listdir(os.path.join(emails_root, 'spam-train'))}
@@ -24,7 +23,7 @@ def add_word_from_class(classname, words):
                     words.add(word)
 
 
-def predict_class(filename, weights, b):
+def predict_class(filename, weights):
     vector = {}
 
     with open(filename) as f:
@@ -41,10 +40,7 @@ def predict_class(filename, weights, b):
         if feature in weights:
             a += weights[feature] * value
 
-    a += b
-
     print([filename, a])
-
 
 
 for classname in files.keys():
@@ -55,7 +51,6 @@ for word in words:
     weights[word] = 0
 
 errors = 0
-max_iter = len(files['nonspam-train']) + len(files['spam-train'])
 
 while True:
     classname = random.choice(files.keys())
@@ -74,29 +69,20 @@ while True:
     for feature, value in features.items():
         a += value * weights[feature]
 
-    a += b
-
     if answers[classname] * a <= 0:
         errors += 1
 
         for feature, value in features.items():
             weights[feature] += value * answers[classname]
 
-        b = b + answers[classname]
+    examples_count += 1
 
     if errors / float(examples_count) <= error_rate:
         break
 
-    examples_count += 1
-
-    print errors / float(examples_count)
-
-# with open('weights.dat', 'rb') as f:
-#     for string in f:
-#         feature, value = string.strip().split('###')
-#         weights[feature] = int(value)
+    print [classname, examples_count, errors / float(examples_count)]
 
 
 for classname in ['spam-test', 'nonspam-test']:
     for filename in os.listdir(os.path.join(emails_root, classname)):
-        predictcted_class = predict_class(os.path.join(emails_root, classname, filename), weights, b)
+        predictcted_class = predict_class(os.path.join(emails_root, classname, filename), weights)
