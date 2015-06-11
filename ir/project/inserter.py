90c73c3c391e3f4d77cdeeb334fea12e8386ef93
Mikhail Usvyatsov <m.usvyatsov@gmail.com>
2015-06-11 21:02:23 +0300

    data collecting

---
+++ ir/project/inserter.py
@@ -0,0 +1,15 @@
+import redis
+
+class Inserter():
+    def __init__(self):
+        self.redis = redis.StrictRedis(host='localhost')
+
+    def insert(self, key, doc_id, tf, docs_number):
+        if self.redis.hexists(key, doc_id):
+            self.redis.hincrby(key, doc_id, tf)
+        else:
+            self.redis.hset(key, doc_id, tf)
+
+
+    def clear_db(self):
+        self.redis.flushdb()
