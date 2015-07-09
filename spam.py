9b3b6fc55a4aafde7d1c263fe23411cb1e8e76d6
Mikhail Usvyatsov <m.usvyatsov@gmail.com>
2015-07-10 03:01:19 +0800

    fixed spam

--- spam.py
+++ spam.py
@@ -1,32 +1,41 @@
  # coding=utf-8
 
-import vk
+import vk_api
 import time
 import random
 
-### You need to create an application in vk. You can do it here:
-### http://vk.com/dev
-
 ### You need to install vk by the following command:
-# pip install vk
+# pip install vk_api
 
-# insert here app id
-app_id = '4855775'
-user_email = ''
+login = ''
 password = ''
 
-vkapi = vk.API(app_id, user_email, password, scope = 'messages')
+vkapi = vk_api.VkApi(login, password, app_id=4855775, scope='messages')
+vkapi.authorization()
 
-friends = vkapi.friends.get(fields = 'first_name')['items']
+friends = vkapi.method('friends.get', {'fields' : 'name'})['items']
 
-start = 0
+friends_count = len(friends)
+start = 457
 
-for i in range(start, len(friends)):
-    first_name = friends[i]['first_name']
-    friend_id = friends[i]['id']
+for i in range(start, friends_count):
+    try:
+        first_name = friends[i]['first_name']
+        friend_id = friends[i]['id']
 
-    message = u'Дорогой {0}. Тут сообщенице быть должно'.format(first_name)
+        message = u'Привет, {0}. Текст.'.format(first_name)
 
-    vkapi.messages.send(user_id = friend_id, message = message)
-    print('{0} out of {1}'.format(i + 1, len(friends)))
-    time.sleep(1 + random.uniform(0, 2))
+        vkapi.method('messages.send', {'user_id' : friend_id, 'message' : message})
+        print('{0} out of {1}'.format(i + 1, friends_count))
+    except Exception:
+        print('retry {0}'.format(i))
+        time.sleep(5)
+
+        try:
+            vkapi.method('messages.send', {'user_id' : friend_id, 'message' : message})
+            print('{0} out of {1}'.format(i + 1, friends_count))
+        except Exception as e:
+            print('skiped {0} because of:'.format(i), e)
+            pass
+    finally:
+        time.sleep(1.5 + random.uniform(0, 2))
